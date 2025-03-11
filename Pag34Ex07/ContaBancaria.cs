170ad9a791f53782c846154a5e07edbd0520f956
Otávio Gonçalves <amanciootavio6@gmail.com>
2025-03-11 12:36:47 -0300

    Add files via upload

---
+++ Pag34Ex07/ContaBancaria.cs
@@ -0,0 +1,22 @@
+﻿namespace Pag34Ex07;
+
+public class ContaBancaria
+{
+    double saldo, saque, deposito;
+
+    public double Saque(double saldo, double saque)
+    {
+        if (saldo > saque)
+        {
+            return saldo - saque;
+        }
+        else
+            return -1;
+    }
+
+    public double Deposito(double saldo, double deposito)
+    {
+
+        return saldo + deposito;
+    }
+}
---
+++ Pag34Ex07/Program.cs
@@ -0,0 +1,34 @@
+﻿using Pag34Ex07;
+
+ContaBancaria contaBancaria = new ContaBancaria();
+
+int numeroDoCartao;
+double saldo, saque, deposito;
+string nomeDoTitular, classificacao;
+
+Console.WriteLine("Qual o nome no seu cartão: ");
+nomeDoTitular = Console.ReadLine();
+
+Console.WriteLine("Qual a sua classificação (comum ou especial): ");
+classificacao = Console.ReadLine();
+
+Console.WriteLine("Qual o numero do seu cartão: ");
+numeroDoCartao = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Qual o seu saldo: ");
+saldo = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Quantos você quer depositar: ");
+deposito = double.Parse(Console.ReadLine());
+
+contaBancaria.Deposito(saldo, deposito);
+
+Console.WriteLine("Quantos você quer sacar: ");
+saque = double.Parse(Console.ReadLine());
+
+contaBancaria.Saque(saldo, saque);
+
+Console.WriteLine($"Nome do titular: {nomeDoTitular} classificação do titular: {classificacao} numero do cartão: {numeroDoCartao} deposito feito na conta: {contaBancaria.Deposito} saque feito na conta: {contaBancaria.Saque}");
+
+
+
